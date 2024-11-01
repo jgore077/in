ae5d46d50b01cc11b0b88f816abe0143533f30b9
James Gore <73591234+jgore077@users.noreply.github.com>
2024-11-01 10:59:34 -0400

    finish build_qrel and add new qrel

--- build_qrel.py
+++ build_qrel.py
@@ -23,60 +23,20 @@ with open(qrel_file,'r',encoding="utf-8") as q:
 with open(topic_file,'r',encoding="utf-8") as t:
     topic = json.loads(t.read())
     
+ids=[obj["Id"] for obj in topic]
+new_qrel=[]
+for entry in qrel:
+    if entry[0] in ids:
+        new_qrel.append(entry)
 
-# The first element in qrel and topic
-print(qrel[:1])
-print(topic[:1])
-
-
-# Filter qrel file, keep only queries present in topic file
-def filter_qrel(topic_file, qrel_file, new_qrel_file):
-
-    with open(topic_file, 'r') as f:
-        topics = json.load(f)
-        topic_ids = set(topic['query_id'] for topic in topics)
-
-    # Write qrel array to the file new_qrel_file in the same format as the qrel_file
-    with open(qrel_file, 'r') as f, open(new_qrel_file, 'w') as out_f:
-        for line in f:
-            query_id, _, _, _ = line.strip().split('\t')
-            if query_id in topic_ids:
-                out_f.write(line)
+with open(new_qrel_file,"w",encoding="utf-8",newline='') as new_qrel_handle :
+    for row in new_qrel:
+        # This is really dumb because I couldnt get the csvwriter working (hate that thing)
+        row=[item+'\t' for item in row[:-1]]+[row[-1]]
+        new_qrel_handle.write(''.join(row))
 
 
-
-
-def qrel_to_tsv(new_qrel_file, tsv_file):
-    """Converts a QREL file to a TSV file."""
-
-    with open(new_qrel_file, 'r') as f, open(tsv_file, 'w', newline='') as tsv_file:
-        writer = csv.writer(tsv_file, delimiter='\t')
-        for line in f:
-            writer.writerow(line.strip().split('\t'))
-
-    # with open(new_qrel_file, 'r') as f:
-    #     lines = f.readlines()
-    #
-    # data = []
-    # for line in lines:
-    #     parts = line.strip().split()
-    #     data.append({
-    #         'query_id': parts[0],
-    #         'iteration': parts[1],
-    #         'doc_id': parts[2],
-    #         'relevance': parts[3]
-    #     })
-    #
-    # df = pd.DataFrame(data)
-    # df.to_csv(tsv_file, sep='\t', index=False)
-
-
-filter_qrel(qrel_file, topic_file, new_qrel_file)
-new_qrel_file = 'qrel.txt'
-tsv_file = 'qrel.tsv'
-qrel_to_tsv(new_qrel_file, tsv_file)
-print(f"Saving TSV file to: {tsv_file}")
-
+print(new_qrel)
 """
 Now that you have the qrel in 2d array format and the topics file as an array of dictionarys you must filter the qrel object
 so that only querys present in the topic2 will remain in the qrel.
